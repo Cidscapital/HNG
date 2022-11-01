4706bdd64342fda9364bdbd49f3ea310977a4658
Cidscapital <72131219+Cidscapital@users.noreply.github.com>
2022-11-02 01:18:40 +0300

    Add files via upload

---
+++ getapi.php
@@ -0,0 +1,7 @@
+<?php
+
+$age = array("slackUsername"=>"Jesse Jason", "Backend"=>True, "age"=>19, "bio"=>"I'm  passionate about web development. Specifically backend. I spend most part of my night coding and listening to lofti music.");
+
+echo json_encode($age);
+
+?>
